8f2bc1b173f19aaeb7c81068fe2c649db459d88f
morgolock <gulfas@gmail.com>
2020-05-08 15:24:38 +0100

    Added script to load new scenes

---
+++ ArgentumOnline/Assets/Scripts/OnEnterLoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class OnEnterLoadScene : MonoBehaviour
+{
+    public string scene = "35";
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("OnEnterLoadScene " + col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        SceneManager.LoadScene(scene);
+    }
+}
