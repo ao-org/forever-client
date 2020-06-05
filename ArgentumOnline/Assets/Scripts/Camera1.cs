57830162db6338b9210cfb587c595bc01ea8940c
pimblack <66213944+pimblack@users.noreply.github.com>
2020-06-04 23:58:33 -0300

    Camera
    zoom in y zoom out de camera por ruedita del mouse

---
+++ ArgentumOnline/Assets/Scripts/Camera1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera1 : MonoBehaviour
+{
+    //private Camera mCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.orthographicSize = 9;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKey(KeyCode.Q)) // Change From Q to anyother key you want
+        {
+            //camera.orthographicSize = camera.orthographicSize + 1 * Time.deltaTime;
+            Camera.main.orthographicSize = Camera.main.orthographicSize - 1 * Time.deltaTime; ;
+            if (Camera.main.orthographicSize < 4)
+            {
+                Camera.main.orthographicSize = 4; // Max size
+            }
+        }
+        if (Input.GetKey(KeyCode.E)) // Change From Q to anyother key you want
+        {
+            Camera.main.orthographicSize = Camera.main.orthographicSize + 1 * Time.deltaTime;
+            if (Camera.main.orthographicSize > 12)
+            {
+                Camera.main.orthographicSize = 12; // Max size
+            }
+        }*/
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            float delta = 1.0f; //Cambiar esta variable para acelerar o no el scroll del mouse
+            Camera.main.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * delta;
+            if (Camera.main.orthographicSize > 12)
+                Camera.main.orthographicSize = 12;
+            if (Camera.main.orthographicSize < 4)
+                Camera.main.orthographicSize = 4;
+        }
+    }
+}
