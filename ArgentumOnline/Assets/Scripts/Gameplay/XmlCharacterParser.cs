f143b5c307261ee3a1569dfa8eef8dfeab278f9d
morgolock <gulfas@gmail.com>
2020-08-06 15:10:34 +0100

    Added fields to xmlcharparser
    Prefab and Npc

--- ArgentumOnline/Assets/Scripts/Gameplay/XmlCharacterParser.cs
+++ ArgentumOnline/Assets/Scripts/Gameplay/XmlCharacterParser.cs
@@ -10,7 +10,7 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Globalization;
 
-public class XmlCharacterParser 
+public class XmlCharacterParser
 {
 
     public XmlCharacterParser(){
@@ -21,28 +21,23 @@ public class XmlCharacterParser
         Debug.Assert(nodes.Count>0);
         foreach (XmlNode nod in nodes)
         {
-
             mName   = nod["name"].InnerText;
             mUUID   = nod["uuid"].InnerText;
-            Debug.Log("Name: " + mName);
+            mPrefab   = nod["prefab"].InnerText;
+            var isNpc = nod.SelectSingleNode("npc");
+            if (isNpc != null)
+                mIsNPC = isNpc.InnerText;
             mMap = nod["position"]["map"].InnerText;
-            Debug.Log("Position Map = " + mMap );
             string xstr = nod["position"]["x"].InnerText;
             string ystr = nod["position"]["y"].InnerText;
-            Debug.Log("Position PosX = " + xstr );
-            Debug.Log("Position PosY = " + ystr );
             float fx = float.Parse(xstr, CultureInfo.InvariantCulture.NumberFormat);
             float fy = float.Parse(ystr, CultureInfo.InvariantCulture.NumberFormat);
             mPos = Tuple.Create(fx,fy);
-            Debug.Log("FPos " + mPos.ToString());
-            //string skinColor = nod["skincolor"].InnerText;
             var skinColor = nod.SelectSingleNode("skincolor");
-            Debug.Log("SKINCOLOR = " + skinColor + "*************************************");
             if (skinColor != null)
                 mSkinColor = skinColor.InnerText;
             else
                 mSkinColor = "5";
-            //string size = nod["size"].InnerText;
         }
     }
 
@@ -60,8 +55,13 @@ public class XmlCharacterParser
     {
         return mSkinColor;
     }
+    public string Prefab(){
+        return mPrefab;
+    }
     private string mName;
     private string mUUID;
+    private string mIsNPC;
+    private string mPrefab;
     private Tuple<float,float> mPos;
     private string mMap;
     private string mSkinColor;
