5cfa53c3b737a7c6ba8e27e0a8ecd0981070fb12
Haracinn <64501140+Haracinn@users.noreply.github.com>
2020-08-13 03:18:44 -0300

    Actualizacion de Mundo
    Se continua con la actualizacion de mundo el reempazo gráfico y la creacion de prefabs

--- ArgentumOnline/Assets/Scripts/Gameplay/MainCamera.cs
+++ ArgentumOnline/Assets/Scripts/Gameplay/MainCamera.cs
@@ -8,7 +8,7 @@ public class MainCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.orthographicSize = 9;
+        Camera.main.orthographicSize = 3;
     }
 
     // Update is called once per frame
@@ -18,10 +18,10 @@ public class MainCamera : MonoBehaviour
         {
             float delta = 1.0f; //Cambiar esta variable para acelerar o no el scroll del mouse
             Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * delta;
-            if (Camera.main.orthographicSize > 9)
-                Camera.main.orthographicSize = 9;
-            if (Camera.main.orthographicSize < 4)
-                Camera.main.orthographicSize = 4;
+            if (Camera.main.orthographicSize > 5)
+                Camera.main.orthographicSize = 5;
+            if (Camera.main.orthographicSize < 1)
+                Camera.main.orthographicSize = 1;
         }
     }
 
