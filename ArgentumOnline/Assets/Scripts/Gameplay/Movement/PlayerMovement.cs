0e484ad26fcfe8e086d49c23e6a26436bf6e37a5
Pablo <gulfas@gmail.com>
2020-08-14 18:54:21 +0100

    Cleaning up

--- ArgentumOnline/Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
+++ ArgentumOnline/Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
@@ -335,16 +335,16 @@ public class PlayerMovement : Movement
 
     }
 
-    private bool IsAnimationPlaying(string anim)
-    {
-        if (mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Sur") || mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Norte") ||
-                mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Oeste") || mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Este") ||
-                mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Noroeste") || mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Noreste") ||
-                mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Sureste") || mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Suroeste"))
-        {
-            return true;
-        }
-        return false;
+    private bool IsAnimationPlaying(string anim){
+        return
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Sur") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Norte") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Oeste") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Este") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Noroeste") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Noreste") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Sureste") ||
+            mAnimator.GetCurrentAnimatorStateInfo(0).IsName(anim + "Suroeste");
     }
 
     private void PlayAnimation(string anim)
