775916dc40b0c031226125c9cd132f788f145a59
Pablo <gulfas@gmail.com>
2020-09-04 14:16:02 +0100

    WIP Histogram

--- ArgentumOnline/Assets/Argentum Forever/Scripts/Networking/Debug/WindowGraph.cs
+++ ArgentumOnline/Assets/Argentum Forever/Scripts/Networking/Debug/WindowGraph.cs
@@ -1,46 +1,84 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WindowGraph : MonoBehaviour
 {
-    [SerializeField] private Sprite mCircleSprite;
+    [SerializeField] private Sprite mSprite;
     private RectTransform mGraphContainer;
-
+    private Texture2D mTexture;
+    private List<GameObject> mBars;
 
     private void Awake(){
+        mTexture = new Texture2D(1,1);
+        mTexture.SetPixel(1,1,new Color(1,1,1,0.5f));
+        mTexture.Apply();
+        mSprite = Sprite.Create(mTexture, new Rect(0, 0, 1, 1), new Vector2(0, 0));
         mGraphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
-        CreateCircle(new Vector2(100,100));
-        List<int> valueList = new List<int>() { 5,98,56,45,30,22,17,15,13,17,25,37,40,36,33};
+        mBars = new List<GameObject>();
+        List<int> valueList = new List<int>() { 5,78,56,45,30,22,17,15,13,17,25,37,40,36,33};
         ShowGraph(valueList);
     }
 
-    private void CreateCircle(Vector2 Pos){
+    private GameObject CreateBar(Vector2 Pos, float barWidth){
         Debug.Assert(mGraphContainer!=null);
-        GameObject gameObject = new GameObject("circle",typeof(Image));
+        float graphHeight = mGraphContainer.sizeDelta.y;
+        GameObject gameObject = new GameObject("bar",typeof(Image));
         gameObject.transform.SetParent(mGraphContainer,false);
-        gameObject.GetComponent<Image>().sprite = mCircleSprite;
+        gameObject.GetComponent<Image>().sprite = mSprite;
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = Pos;
-        rectTransform.sizeDelta = new Vector2(11,11);
+        rectTransform.sizeDelta = new Vector2(barWidth, graphHeight- (graphHeight-Pos.y));
         rectTransform.anchorMin = Vector2.zero;
         rectTransform.anchorMax = Vector2.zero;
-
-
+        return gameObject;
     }
-
-
-
-
     private void ShowGraph(List<int> valueList){
-        float xSize =50f;
+        float barWidth = 10f;
+        float xSep = 5f;
+        float xSize =20f;
         float yMaximum =200f;
         float graphHeight = mGraphContainer.sizeDelta.y;
+        Debug.Log("graphHeight " + mGraphContainer.sizeDelta.y);
+        GameObject lastCircle = null;
         for(int i=0; i< valueList.Count;++i){
-            float xPosition = i * xSize;
+            float xPosition = xSize + i * xSize ;
             float yPosition = (valueList[i]/yMaximum)*graphHeight;
-            CreateCircle(new Vector2(xPosition,yPosition));
+            mBars.Add(CreateBar(new Vector2(xPosition,yPosition),barWidth));
+            /*
+            if(lastCircle!=null){
+                CreateDotConnection(lastCircle.GetComponent<RectTransform>().anchoredPosition,
+                    circleGameObj.GetComponent<RectTransform>().anchoredPosition);
+
+
+            }
+            lastCircle = circleGameObj;
+            */
         }
+    }
+    void Update()
+    {
+        /*
+        for (var i = 0; i < mBars.Count; i++) {
+                Destroy(mBars[i]);
+        }
+        List<int> valueList = new List<int>();
+        for(int i=0; i< 20; ++i){
+            valueList.Add(Random.Range(5, 100));
+        }
+        ShowGraph(valueList);
+        */
+    }
+
+    private void CreateDotConnection(Vector2 dotPositionA,Vector2 dotPositionB){
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(mGraphContainer,false);
+        gameObject.GetComponent<Image>().color = new Color(1,1,1,0.5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(100,3f);
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.zero;
+        rectTransform.anchoredPosition = dotPositionA;
     }
 }
