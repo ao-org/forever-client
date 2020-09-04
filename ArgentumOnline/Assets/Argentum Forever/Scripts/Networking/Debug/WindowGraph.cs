40c182dc2bd8155e2715da6f549e10b7feb9a1aa
Pablo <gulfas@gmail.com>
2020-09-04 22:09:30 +0100

    WIP Histogram

--- ArgentumOnline/Assets/Argentum Forever/Scripts/Networking/Debug/WindowGraph.cs
+++ ArgentumOnline/Assets/Argentum Forever/Scripts/Networking/Debug/WindowGraph.cs
@@ -14,26 +14,27 @@ public class WindowGraph : MonoBehaviour
 
     private void Awake(){
         mTexture = new Texture2D(1,1);
-        mTexture.SetPixel(1,1,new Color(1,1,1,0.5f));
+        mTexture.SetPixel(1,1,new Color(0,1,1,0.3f));
         mTexture.Apply();
         mSprite = Sprite.Create(mTexture, new Rect(0, 0, 1, 1), new Vector2(0, 0));
         mGraphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
         mBars = new List<GameObject>();
-        mValues = new List<int>() { 5,78,56,45,30,22,17,15,13,17,25,37,40,36,33};
+        mValues = new List<int>() { 5,78,56,45,30,22,17,15,13,17,5,78,56,45,30,22,17,15,13,17,25,37,40,36,33, 5,78,56,45,30,22,17,15,13,17,25,37,40,36,33};
         ShowGraph(mValues);
         mInputStopwatch = new System.Diagnostics.Stopwatch();
         mInputStopwatch.Start();
     }
 
-    private GameObject CreateBar(Vector2 Pos, float barWidth){
+    private GameObject CreateBar(Vector2 Pos, float barWidth, float barHeight){
         Debug.Assert(mGraphContainer!=null);
         float graphHeight = mGraphContainer.sizeDelta.y;
         GameObject gameObject = new GameObject("bar",typeof(Image));
         gameObject.transform.SetParent(mGraphContainer,false);
         gameObject.GetComponent<Image>().sprite = mSprite;
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = Pos;
-        rectTransform.sizeDelta = new Vector2(barWidth, graphHeight- (graphHeight-Pos.y));
+        rectTransform.pivot = Vector2.zero;
+        rectTransform.anchoredPosition = new Vector2(Pos.x, -Pos.y);
+        rectTransform.sizeDelta = new Vector2(barWidth, barHeight);
         rectTransform.anchorMin = Vector2.zero;
         rectTransform.anchorMax = Vector2.zero;
         return gameObject;
@@ -47,17 +48,9 @@ public class WindowGraph : MonoBehaviour
         GameObject lastCircle = null;
         for(int i=0; i< valueList.Count;++i){
             float xPosition = xSize + i * xSize ;
-            float yPosition = (valueList[i]/yMaximum)*graphHeight;
-            mBars.Add(CreateBar(new Vector2(xPosition,yPosition),barWidth));
-            /*
-            if(lastCircle!=null){
-                CreateDotConnection(lastCircle.GetComponent<RectTransform>().anchoredPosition,
-                    circleGameObj.GetComponent<RectTransform>().anchoredPosition);
-
-
-            }
-            lastCircle = circleGameObj;
-            */
+            float yPosition = 0; //(valueList[i]/yMaximum)*graphHeight;
+            float barHeight = (valueList[i]/yMaximum)*graphHeight;
+            mBars.Add(CreateBar(new Vector2(xPosition,yPosition),barWidth,barHeight));
         }
     }
     void FixedUpdate()
